fee4e63dd4308bda99c05ceb123a30df54f7ecf6
selinbulbul <buelbuel.selin@gmail.com>
2021-07-07 17:35:49 +0200

    save

--- DSLR_Cameras/create_search_urls.py
+++ DSLR_Cameras/create_search_urls.py
@@ -1,11 +1,11 @@
 f = open('search_urls.txt', 'w+')
-url = 'https://www.amazon.de/s?k=DSLR+Cameras'
+url = 'https://www.amazon.de/s?k=DSLR&i=electronics&ref=nb_sb_noss_2'
 f.write(url)
 f.write('\n')
 
 
-for i in range(2,21):
-    url = 'https://www.amazon.de/-/en/s?k=DSLR+Cameras&page=' + str(i) + '&qid=1625555168&ref=sr_pg_' + str(i)
+for i in range(2,70):
+    url = 'https://www.amazon.de/-/en/s?k=DSLR&i=electronics&page=' + str(i) + '&qid=1625668687&ref=sr_pg_' + str(i)
     f.write(url)
     f.write('\n')
 
