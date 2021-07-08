52eb829a29d4be6658dffcf940fd0fcad459a80f
selinbulbul <buelbuel.selin@gmail.com>
2021-07-08 10:56:28 +0200

    save

--- processor/create_search.urls.py
+++ processor/create_search.urls.py
@@ -1,11 +1,11 @@
 f = open('search_urls.txt', 'w+')
-url = 'https://www.amazon.com/s?k=Processor&ref=nb_sb_noss_2'
+url = 'https://www.amazon.de/s?k=processor&i=computers&ref=nb_sb_noss_2'
 f.write(url)
 f.write('\n')
 
 
-for i in range(2,51):
-    url = 'https://www.amazon.com/s?k=Processor&i=computers-intl-ship&page=' + str(i) + '&qid=1594555641&ref=sr_pg_' + str(i)
+for i in range(2,72):
+    url = 'https://www.amazon.de/-/en/s?k=processor&i=computers&page=' + str(i) + '&qid=1625731797&ref=sr_pg_' + str(i)
     f.write(url)
     f.write('\n')
 f.close()
